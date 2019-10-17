d3b8aca7244a6ea73062f7aedff917f6771b8f16
taylor <tvories@gmail.com>
2019-10-17 08:35:01 -0600

    Some k8 services added and storage classes

---
+++ kubernetes/cert-manager/prereqs.sh
@@ -0,0 +1,19 @@
+# Install the CustomResourceDefinition resources separately
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
+
+# Create the namespace for cert-manager
+kubectl create namespace cert-manager
+
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
+helm install \
+  --name cert-manager \
+  --namespace cert-manager \
+  --version v0.11.0 \
+  --set 'extraArgs={--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53}' \
+  jetstack/cert-manager
