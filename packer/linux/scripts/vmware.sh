3b3e80407744dce2b02cf7f2c3073a4e301ab832
Taylor Vories <taylor@tmtech.me>
2019-11-25 19:47:37 -0700

    Updated packer configs

--- packer/linux/scripts/vmware.sh
+++ packer/linux/scripts/vmware.sh
@@ -9,12 +9,3 @@ fi
 
 sudo apt-get -y install open-vm-tools
 
-# Ensure config file exists
-sudo touch /etc/vmware-tools/tools.conf
-
-# Tell open vm tools to ignore other NICs
-grep -qFs 'exclude-nics=' /etc/vmware-tools/tools.conf || sudo tee -a /etc/vmware-tools/tools.conf <<EOF
-# Disable additional nics from reporting IP
-[guestinfo]
-exclude-nics=docker*,veth*,br*
-EOF
